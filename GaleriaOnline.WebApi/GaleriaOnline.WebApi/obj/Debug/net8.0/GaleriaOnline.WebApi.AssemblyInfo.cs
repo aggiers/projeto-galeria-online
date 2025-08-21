93cd17c12670c8d12f13c125a7b1e9fd81d5d021
Agatha <agatha.g@aluno.senai.br>
2025-08-21 09:00:14 -0300

    feat: finalizando api e mandando para as branchs

--- GaleriaOnline.WebApi/GaleriaOnline.WebApi/obj/Debug/net8.0/GaleriaOnline.WebApi.AssemblyInfo.cs
+++ GaleriaOnline.WebApi/GaleriaOnline.WebApi/obj/Debug/net8.0/GaleriaOnline.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GaleriaOnline.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+686c1bbba35d24597aac00321c573ba8175dc041")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1d12fe8a19c9c9d63f4d7fea4d56ff51814fc0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GaleriaOnline.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GaleriaOnline.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
